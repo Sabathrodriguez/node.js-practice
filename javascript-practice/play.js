ef3cfa99bc2e5c7f4df6a93f4e40e8e52912f197
sabathrodriguez <sabathcasillas1@gmail.com>
2019-12-18 10:29:56 -0700

    node.js practice and web server manual setup

---
+++ javascript-practice/play.js
@@ -0,0 +1,41 @@
+// const person = {
+//     name: 'sabath',
+//     age: 23
+// }
+
+// const tempPerson = {...person};
+// console.log(tempPerson);
+
+
+
+// const hobbies = ['sports', 'reading'];
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+
+// console.log(hobbies.map(hobby => {
+//     return 'hobby: ' + hobby;
+// }));
+// console.log(hobbies);
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
+
+
+// const printName = ({ name }) => {
+//     console.log(name);
+// }
+
+// printName(person);
+
+// const {name, age} = person;
+// console.log(name, age);
+
+// const [hobby1, hobby2] = hobbies;
+
+setTimeout(() => {
+    console.log("timer is done")
+}, 2000);
+
+console.log("Hello");
+console.log("Hi")
+
